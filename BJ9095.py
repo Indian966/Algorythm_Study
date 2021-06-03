784dd1bb3d66bc0828f4bceb6405dadaba5c8bcb
Indian966 <rlaalsrb4175@gmail.com>
2021-06-03 17:54:48 +0900

    DP 풀이

--- BJ9095.py
+++ BJ9095.py
@@ -1,13 +1,27 @@
-#T = int(input())
-n = 4
-
+# T = int(input())
+# arr = [int(input()) for _ in range(T)]
 
 def dp(m) :
-    memo = [0 for _ in range(m + 1)]
-    coin = [1,2,3,4]
+    memo = [0]
+    memo.append(1)
+    memo.append(2)
+    memo.append(4)
+
+
+    if m > 3 :
+        for _ in range(4, m + 1):
+            memo.append(0)
+        for i in range(4, m + 1):
+            memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3]
+
+    print(memo[m])
+
+# for n in arr:
+#     dp(n)
+dp(7)
 
 
 # dp(1) = 1, dp(2) = 2, dp(3) = 1+1+1,1+2,2+1,3 = 4
 # dp(4) = 7 = dp(3) + 3, dp(5) = 13 = dp(4) + dp(3) + dp(2)
 # dp(6) = 24 = dp(5) + dp(4) + dp(3), dp(7) = 44
-1 2 4 7 13 24 44
+# 1 2 4 7 13 24 44
