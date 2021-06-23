672fcf474821be84f5346f327fc4ca724ac67499
Indian966 <rlaalsrb4175@gmail.com>
2021-06-23 15:16:00 +0900

    BJ1260_DFSNBFS.py BJ2178_Maze.py

---
+++ BJ1260_DFSNBFS.py
@@ -0,0 +1,29 @@
+N, M, V = map(int, input().split())
+graph = [[0]*(N+1) for _ in range(N+1)]
+visited = [False] * (N+1)
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+def dfs(v):
+    visited[v] = True
+    print(v, end=' ')
+    for i in range(1, N + 1):
+        if not visited[i] and graph[v][i] == 1:
+            dfs(i)
+
+def bfs(v):
+    queue = [v]
+    visited[v] = False
+    # dfs 실행 시에 True로 방문처리 되어있기 때문에, 여기선 False로 방문처리
+    while queue:
+        v = queue.pop(0)
+        print(v, end=' ')
+        for i in range(1, N+1):
+            if visited[i] and graph[v][i] == 1:
+                queue.append(i)
+                visited[i] = False
+dfs(V)
+print() 
+bfs(V)
+
