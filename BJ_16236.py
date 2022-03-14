9a2d277e40a5f6add0b45633fa1fd7541cfa1832
Indian966 <rlaalsrb4175@gmail.com>
2022-03-14 16:52:08 +0900

    BJ_16236.py

--- BJ_16236.py
+++ BJ_16236.py
@@ -1,5 +1,26 @@
 n = int(input())
 graph = []
 for _ in range(n) :
-    temp = list(map(int,input()))
+    temp = list(map(int,input().split()))
     graph.append(temp)
+
+print(n, graph)
+
+for i in graph :
+    if 9 in i :
+        x = graph.index(i)
+        y = i.index(9)
+
+print(x,y)
+
+size = 2
+time = 0
+list = []
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] > 0 and graph[i][j] < size :
+            dist = abs((x-i)+(y-j))
+            list.append({dist : i,j})
+print(list)
+# fish = 0
+# if fish < size :
