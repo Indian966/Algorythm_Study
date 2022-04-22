957a62c8e5e7725c7643fac13587c3b111f9946b
Indian966 <rlaalsrb4175@gmail.com>
2022-04-22 16:01:33 +0900

    그래프와 bfs 기초 다지기

--- pr49189.py
+++ pr49189.py
@@ -1,26 +1,40 @@
 # 노드의 개수 n, 간선에 대한 정보가 담긴 2차원 배열 vertex가 매개변수로 주어질 때,
 # 1번 노드로부터 가장 멀리 떨어진 노드가 몇 개인지를 return 하도록 solution 함수를 작성해주세요.
 
+from collections import deque
+
 n = 6
 vertex = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
 # return 3
 
 
 def solution(n, edge):
-    start, answer = 0
-    visited = [0] * n
-    queue = []
-    edge.sort(key=lambda x:x[0])
-    def bfs (start) :
-        queue.append(start)
-        while queue :
-            s = queue.pop(0)
-            if visited[s] == 0 :
-                visited[s] = 1
-            for index in range(len(edge)) :
-                if s in edge[index] and visited[index] == 0 :
-                    queue.append(index)
+    answer = 0
+    graph = [[] for _ in range(n + 1)]
+    for s, e in edge:  # 그래프에 현재 노드와 연결되어있는 노드 저장
+        graph[s].append(e)
+        graph[e].append(s)
+
+    for e in graph:
+        e.sort()
+
+    def bfs (n) :
+        visited = [0 for _ in range(n+1)]
+        visited[1] = 1
+        q = deque()
+        q.append((1, 0))  # 시작 노드, 거리
 
 
+        while q:
+            node, dist = q.popleft()
+            for i in graph[node] :
+                if visited[i] == 0 :
+                    visited[i] = dist + 1
+                    q.append((i, dist + 1))
 
-    return answer
+        max_val = max(visited)
+        return visited.count(max_val)
+
+    return bfs(n)
+
+print(solution(n,vertex))
