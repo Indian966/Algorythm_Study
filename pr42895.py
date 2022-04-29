ff5722720ff60250090d699e85d255ffaaf42ed9
Indian966 <rlaalsrb4175@gmail.com>
2022-04-29 14:35:44 +0900

    pr42895.py dp 기초다지기

--- pr42895.py
+++ pr42895.py
@@ -11,13 +11,20 @@
 # 2	11	    3
 
 def solution(N, number):
-    answer = 0
-    def dp (N, number) :
-
-        return 0
-    arr = []
-
-
-
+    answer = -1
+    dp  =[set([N*int('1'*i)]) for i in range(1, 9)]
+    for i in range(8) :
+        for j in range(i):
+            for num1 in dp[j]:
+                for num2 in dp[i - j - 1]:
+                    dp[i].add(num1 + num2)
+                    dp[i].add(num1 - num2)
+                    dp[i].add(num1 * num2)
+                    if num2 != 0:
+                        dp[i].add(num1 // num2)
+        if number in dp[i] :
+            return i+1
 
     return answer
+
+print(solution(5,12))
