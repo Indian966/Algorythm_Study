f2bcf2e25708f93d02703d9212b3ffe2696fbf84
Indian966 <rlaalsrb4175@gmail.com>
2022-05-12 18:07:27 +0900

    pr42860_greedy_조이스틱.py qt같은 문제

--- Programmers/pr42860_greedy_조이스틱.py
+++ Programmers/pr42860_greedy_조이스틱.py
@@ -2,19 +2,28 @@
 # 이름에 대해 조이스틱 조작 횟수의 최솟값을 return 하도록 solution 함수를 만드세요.
 
 
-name = 'JEROEN'
+name = 'JAZ'
 
 def solution(name):
     answer = 0
-
+    start = 0
+    count = 0
+    dist = []
     for i in range(len(name)) :
         if name[i] != 'A' :
             # print(ord('A'))
             # print(ord(name[i]))
             right = ord(name[i]) - 65
             left = 90 - ord(name[i]) + 1
-            print(name[i], min(left, right))
-            # print(name[i], left, right)
+            if left < right :
+                dist.append(left)
+            else:
+                dist.append(right)
+        else:
+            dist.append(0)
+
+
+
 
 
     return answer
