f5134990e6c0a1680f6d037153c68f1f3588eb57
Indian966 <rlaalsrb4175@gmail.com>
2022-10-06 23:26:40 +0900

    코딩테스트

---
+++ CodingTest/1006위시캣01.py
@@ -0,0 +1,15 @@
+# 사내의 팀원들 전체를 대상으로 랜덤한 조를 구성하려고 합니다. 한 조는 최소 5명, 최대 7명으로
+# 구성하고, 조의 개수는 최소화해야 합니다
+
+def mix_members (members) :
+    answer = []
+
+
+
+    return answer
+
+m = [5, 7, 10, 22]
+# 이름 대신 숫자
+members = [i for i in range(m)]
+
+print(mix_members(members))
