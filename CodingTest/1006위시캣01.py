a4445f934ac891f7cb85aa6a775caf3b4a48ce2b
Indian966 <rlaalsrb4175@gmail.com>
2022-10-09 13:10:43 +0900

    코딩테스트

--- CodingTest/1006위시캣01.py
+++ CodingTest/1006위시캣01.py
@@ -4,47 +4,86 @@
     핵심은 팀원의 숫자를 최대한 7명 단위로 쪼개면서
     나머지 인원들을 6 내지 5명으로 구성하는 것
 """
+from collections import defaultdict
+import random
 
-
-
-def mix_members (members) :
+def mix_members (mem_numb) :
     answer = []
+    member_list = [i for i in range(mem_numb)]
+    member_dict = defaultdict(list)
 
-    numb = members
-    mok_7, r = divmod(numb, 7)
-    mok_6 = 0
-    mok_5 = 0
+    div_seven = mem_numb // 7
 
-    # 70명 이하라면 그냥
+    # 70명 미만일때는 5명으로 우선 나누고 나머지를 각 조에 뿌리는게 편함
+    if div_seven < 10 :
+        for i in range(1, 11):
+            for _ in range(5):
+                out_mem = random.choice(member_list)
+                member_list.remove(out_mem)
+                member_dict[f'{i}조'].append(out_mem)
 
+            # 나머지 멤버 넣어주기
+            group_cnt = 1
+            while member_list:
+                if len(member_dict[f'{group_cnt}조']) < 7:
+                    out_mem = random.choice(member_list)
+                    member_list.remove(out_mem)
+                    member_dict[f'{group_cnt}조'].append(out_mem)
+                else:
+                    group_cnt += 1
+    else :
+        numb = mem_numb
+        mok_7, r = divmod(numb, 7)
+        mok_6 = 0
+        mok_5 = 0
 
-    # 총원을 7로 나눈 나머지가 6의 배수인지, 5의 배수인지 검사 후
-    # 맞다면
-    while True:
-        # 6의 배수가 아니면서 5의 배수일 때
-        if (r % 6) > 0 and not (r % 6) % 5:
-            q_6 = r // 6
-            q_5 = r % 6 // 5
-            break
-        # 6의 배수일 때
-        elif not r % 6:
-            q_6 = r // 6
-            break
-        elif not r % 5:
-            q_5 = r // 5
-            break
+        # 70명 이하라면 그냥
 
-        mok_7 -= 1
-        r += 7
-    print(mok_7, mok_6, mok_5)
+        # 총원을 7로 나눈 나머지가 6의 배수인지, 5의 배수인지 검사 후
+        # 맞다면
+        while True:
+            # 6의 배수가 아니면서 5의 배수일 때
+            if (r % 6) > 0 and not (r % 6) % 5:
+                q_6 = r // 6
+                q_5 = r % 6 // 5
+                break
+            # 6의 배수일 때
+            elif not r % 6:
+                q_6 = r // 6
+                break
+            elif not r % 5:
+                q_5 = r // 5
+                break
 
-    return answer
+            mok_7 -= 1
+            r += 7
 
-# m = [5, 7, 10, 22]
-# # 이름 대신 숫자
-# members = [i for i in range(m)]
+        # 7명 조의 숫자만큼 딕셔너리에 추가
+        group_cnt = 1
+        for _ in range(1, mok_7 + 1):
+            for _ in range(7):
+                out_mem = random.choice(member_list)
+                member_list.remove(out_mem)
+                member_dict[f'{group_cnt}조'].append(out_mem)
+            group_cnt += 1
 
-# print(mix_members(members))
+        # 6명 조의 숫자만큼 딕셔너리에 추가
+        for _ in range(1, mok_6 + 1):
+            for _ in range(6):
+                out_mem = random.choice(member_list)
+                member_list.remove(out_mem)
+                member_dict[f'{group_cnt}조'].append(out_mem)
+            group_cnt += 1
+
+        # 5명 조의 숫자만큼 딕셔너리에 추가
+        for _ in range(1, mok_5 + 1):
+            for _ in range(5):
+                out_mem = random.choice(member_list)
+                member_list.remove(out_mem)
+                member_dict[f'{group_cnt}조'].append(out_mem)
+            group_cnt += 1
+
+    return member_dict
 
 
 # result 18 1 0
